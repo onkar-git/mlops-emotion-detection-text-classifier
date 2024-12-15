1fe6311a452a3bb3a52a82dd8fbacff666de26a1
onkar-git <om.8589@gmail.com>
2024-12-15 17:16:52 +0530

    added flask code and unit tetst related to flask

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,53 +3,103 @@ from preprocessing_utility import normalize_text
 import dagshub
 import mlflow
 import pickle
+import os 
+import pandas
+
+
+# Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "onkar-git"
+repo_name = "mlops-emotion-detection-text-classifier"
+
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 app =Flask(__name__)
 
 #load model from mlflow model registry
-mlflow.set_tracking_uri('https://dagshub.com/onkar-git/mlops-emotion-detection-text-classifier.mlflow')
-dagshub.init(repo_owner='onkar-git', repo_name='mlops-emotion-detection-text-classifier', mlflow=True)
+#mlflow.set_tracking_uri('https://dagshub.com/onkar-git/mlops-emotion-detection-text-classifier.mlflow')
+#dagshub.init(repo_owner='onkar-git', repo_name='mlops-emotion-detection-text-classifier', mlflow=True)
+
+
+
+
+
+# model_name = "my_model"
+# model_version = 5
+
+# model_uri = f'models:/{model_name}/{model_version}'
+
+# model = mlflow.pyfunc.load_model(model_uri)
+
+# vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
+
+
+
+# Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "campusx-official"
+repo_name = "mlops-project-2"
+
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+
+app = Flask(__name__)
+
+# load model from model registry
+def get_latest_model_version(model_name):
+    client = mlflow.MlflowClient()
+    latest_version = client.get_latest_versions(model_name, stages=["Production"])
+    if not latest_version:
+        latest_version = client.get_latest_versions(model_name, stages=["None"])
+    return latest_version[0].version if latest_version else None
 
 model_name = "my_model"
-model_version = 5
+model_version = get_latest_model_version(model_name)
 
 model_uri = f'models:/{model_name}/{model_version}'
-
 model = mlflow.pyfunc.load_model(model_uri)
 
-vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
-
-
+vectorizer = pickle.load(open('models/vectorizer.pkl','rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html',result=None)
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    text=request.form['text']
 
-    #load model from model regstry
+    text = request.form['text']
 
-
-    # clean 
+    # clean
     text = normalize_text(text)
 
+    # bow
     features = vectorizer.transform([text])
-     
-    #BOW
 
-    result = model.predict(features)
+    # Convert sparse matrix to DataFrame
+    features_df = pd.DataFrame.sparse.from_spmatrix(features)
+    features_df = pd.DataFrame(features.toarray(), columns=[str(i) for i in range(features.shape[1])])
 
-    return render_template('index.html',result=str(result[0]))
-    
+    # prediction
+    result = model.predict(features_df)
 
+    # show
+    return render_template('index.html', result=result[0])
 
-    #prediction
-
-
-
-
-    #show  predictio
-
-app.run(debug=True,port=8000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
